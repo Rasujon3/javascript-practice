4db544bd65194b564a6431f9f8b7d28ee847c9a4
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-05-29 21:24:28 +0600

    01.RE - Introduction - JavaScript by Simanta Paul
    01.RE - Introduction - JavaScript by Simanta Paul

--- script2.js
+++ script2.js
@@ -1,74 +1,42 @@
-// Events of Javascript
-// event w3school
-// Onclick
+// Regular Expression
+// Pattern Matching Technique
 
-// Event listener
+let re;
+let str;
 
-// function message() {
-//     console.log("Mouse over button");
-// }
+re = /hello/;
+re = /Hello/;
+re = /hello/i; // i = case insensitive
 
-// let btn = document.getElementById('sample-btn').addEventListener
-// ('click', message);
+console.log(re);
+console.log(re.source);
 
-// function message(e) {
-//     let val = e;
-//     val = e.target;
-//     val = e.target.id;
+str = "Hello World!";
+str = "Again Hello World!";
+str = "Hell World!";
+str = "sfsdHellofsd World!";
+str = "Again Hello World! hello";
+str = "World!";
 
-//     val = e.timeStamp;
-//     val = e.type;
+// exec() - Returns result in an array or null
+let result = re.exec(str);
 
-//     val = e.clientY;
-//     val = e.clientX;
+// test() - true/false
+result = re.test(str);
 
-//     val = e.offsetY;
-//     val = e.offsetX;
+// match() - Returns array or null
+str = "Again Hello World! hello";
+result = str.match(re);
 
-//     console.log(val);
-// }
+// search() - Returns index of the first match or -1
+str = "World";
+result = str.search(re);
 
-// let btn = document.querySelector(".container").addEventListener
-// ('mousemove', message);
+// replace() - Return new string
+str = "Again Hello World! hello";
+let newstr = str.replace(re , "hi");
 
-// document.querySelector('.container').style.background = "red";
-
-// function message(e) {
-//     let val = e;
-//     val = e.target;
-//     val = e.target.id;
-
-//     val = e.timeStamp;
-//     val = e.type;
-
-//     val = e.clientY;
-//     val = e.clientX;
-
-//     val = e.offsetY;
-//     val = e.offsetX;
-//     val = this;
-
-//     this.style.background = `#${e.offsetX}`;
-
-//     // console.log(val);
-//     console.log(e.offsetX);
-//     console.log(e.offsetY);
-//     console.log(val);
-// }
-
-// console.log(btn);
-
-let btn = document.querySelector("#name").addEventListener
-('focus', test);
-
-document.querySelector("#name").addEventListener('keyup', test2)
-
-function test(e) {
-    this.style.background = 'pink';
-}
-function test2(e) {
-    // console.log(this.value);
-    document.getElementById("demo").innerText = this.value;
-}
-
-
+console.log(result);
+console.log(str);
+console.log(re.source);
+console.log(newstr);
